a471c120faf9fb783055d880e85f580d0e8e1674
Jianshi Huang <jianshi.huang@gmail.com>
2013-03-17 16:09:22 +0800

    up

--- huangjs/words.lisp
+++ huangjs/words.lisp
@@ -3,10 +3,20 @@
 (defword 2 + (x y)
   (+ x y))
 
+(defword 2 - (x y)
+  (- x y))
+
 (defword 0 bye ()
   (throw 'exit nil))
 
 (defword 0 .s ()
   (prin1 *d-stack*)
   (terpri))
+
+(defword 0 pop ()
+  (pop *d-stack*))
+
+(defword 1 push (x)
+  ;; FIXME:
+  (push x *d-stack*))
 
